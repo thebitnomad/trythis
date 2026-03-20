5e2dd6d767107f182a122f1affc4a0d21effdd07
Ivan Schroll <bitnomad@gmx.com>
2026-03-20 01:37:19 -0300

    upp

---
+++ src/Types/meta.inc__libsignal.d.ts
@@ -0,0 +1,4 @@
+declare module '@meta.inc/libsignal'
+declare module '@meta.inc/libsignal/src/curve'
+declare module '@meta.inc/libsignal/src/crypto'
+declare module '@meta.inc/libsignal/src/protobufs'
